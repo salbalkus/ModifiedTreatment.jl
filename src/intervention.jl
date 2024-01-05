48f4fbc5d9874de570c2a8d2073b11448b0757a8
Salvador Balkus <salbalkus@gmail.com>
2024-01-05 11:30:07 -0500

    change mljbase to mmi

--- src/intervention.jl
+++ src/intervention.jl
@@ -5,5 +5,5 @@ mutable struct ShiftIntervention <: Intervention
     ShiftIntervention(func) = applicable(func, Vector, Float64) ? new(func) : error("ShiftIntervention: func must be a function of type (Vector{Float64}, Float64) -> Vector{Float64}")
 end
 
-MLJBase.transform(model::ShiftIntervention, _, ct, δ) = A .+ δ
-MLJBase.inverse_transform(model::ShiftIntervention, _, ct, δ) = A .- δ
+MMI.transform(model::ShiftIntervention, _, ct, δ) = A .+ δ
+MMI.inverse_transform(model::ShiftIntervention, _, ct, δ) = A .- δ
